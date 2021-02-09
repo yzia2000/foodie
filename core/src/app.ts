3bbdd042b8e03a4e191794899608df0f39a7d69c
yzia2000 <e0381439@u.nus.edu>
2021-02-10 02:19:10 +0530

    created base repo

--- core/src/app.ts
+++ core/src/app.ts
@@ -1,8 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 
-const PORT: string = process.env.PORT || "5000";
-
 const app = express();
 
 app.use(cors());
@@ -11,6 +9,4 @@ app.get('/', (_req, res) => {
     res.send("Server is up");
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on PORT ${PORT}`);
-});
+export default app;
