c51a28623abab0dead03f712f792f4ab36ab7034
yzia2000 <e0381439@u.nus.edu>
2021-02-16 21:52:44 +0530

    Unauth ssl reject false

--- core/src/db.ts
+++ core/src/db.ts
@@ -1,9 +1,11 @@
 import { Pool } from 'pg';
 
 const connectionString: string =
-  process.env.DATABASE_URL ??
-  'postgresql://postgres:postgres@db:5432/database';
+  process.env.DATABASE_URL ?? 'postgresql://postgres:postgres@db:5432/database';
 
 export default new Pool({
   connectionString: connectionString,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
