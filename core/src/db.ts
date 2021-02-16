add17727012bc36a82653f481a1364ad14e4da31
yzia2000 <e0381439@u.nus.edu>
2021-02-16 22:11:41 +0530

    SSL mode require in production

--- core/src/db.ts
+++ core/src/db.ts
@@ -1,11 +1,10 @@
 import { Pool } from 'pg';
 
 const connectionString: string =
-  process.env.DATABASE_URL ?? 'postgresql://postgres:postgres@db:5432/database';
+  (process.env.DATABASE_URL ??
+    'postgresql://postgres:postgres@db:5432/database') +
+  (process.env.NODE_ENV === 'production' ? '?sslmode=require' : '');
 
 export default new Pool({
   connectionString: connectionString,
-  ssl: {
-    rejectUnauthorized: false,
-  },
 });
